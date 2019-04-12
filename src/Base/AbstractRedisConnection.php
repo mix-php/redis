36e7498b6bc20bd59900bcb97bbefd31504ea101
ihipop <ihipop@users.noreply.github.com>
2019-04-12 14:50:34 +0800

    当没设置Redis密码的时候，不发送密码认证

--- src/Base/AbstractRedisConnection.php
+++ src/Base/AbstractRedisConnection.php
@@ -55,7 +55,9 @@ abstract class AbstractRedisConnection extends AbstractComponent implements Redi
         if (!$redis->connect($this->host, $this->port)) {
             throw new \RedisException("Redis connection failed, host: {$this->host}, port: {$this->port}");
         }
-        $redis->auth($this->password);
+        if ('' != (string)$this->password) {//假设密码是字符串 0 也能通过这个校验
+            $redis->auth($this->password);
+        }
         $redis->select($this->database);
         return $redis;
     }
